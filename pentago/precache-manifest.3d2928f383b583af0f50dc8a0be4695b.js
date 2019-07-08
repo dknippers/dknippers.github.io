dd6871ce229c7f97e348099a1b1968eb2449d4b0
Daniël Knippers <dknippers@gmail.com>
2019-07-08 20:00:41 +0200

    Updated to last version of Pentago, fixing all reported vulnerabilities by npm audit.

---
+++ pentago/precache-manifest.3d2928f383b583af0f50dc8a0be4695b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ef1610ae990d2e4edcf3a8ea78dccb",
+    "url": "/pentago/index.html"
+  },
+  {
+    "revision": "0aaac772acbfc05b1aab",
+    "url": "/pentago/static/css/main.6e63de31.chunk.css"
+  },
+  {
+    "revision": "18f845fd3a04c7d9c841",
+    "url": "/pentago/static/js/2.642df18d.chunk.js"
+  },
+  {
+    "revision": "0aaac772acbfc05b1aab",
+    "url": "/pentago/static/js/main.7c2df92b.chunk.js"
+  },
+  {
+    "revision": "da2199979de25733f942",
+    "url": "/pentago/static/js/runtime~main.3aaa5e29.js"
+  },
+  {
+    "revision": "942198c7d12c1263322f0b04ad600686",
+    "url": "/pentago/static/media/clockwise.942198c7.svg"
+  },
+  {
+    "revision": "d21e64ebb989e27f93b5eff30074f70b",
+    "url": "/pentago/static/media/counter-clockwise.d21e64eb.svg"
+  },
+  {
+    "revision": "bd1a04209f56b830051f4317b3a6bf7b",
+    "url": "/pentago/static/media/fiene.bd1a0420.png"
+  }
+]);
